499e97bad23943c316d34285527c7b735706d2ac
SahilKamate <sahilkamate03@gmail.com>
2022-07-25 16:33:09 +0530

    more features added

--- python_project/todo/model.py
+++ python_project/todo/model.py
@@ -1,10 +1,24 @@
-from app import db
+from todo import db
 from datetime import datetime
 
-class todo(db.Model):
-    id = db.Column(db.Interger, primary_key=True)
-    title = db.Column(db.String(50), nullable=False)
-    time_posted = db.Column(db.DateTime, default=datetime.utcnow())
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    todos = db.relationship('Todo', backref='author')
 
     def __repr__(self) -> str:
-        return f'Todo ({self.title}, {self.time_posted})'
+        return f'User ({self.username}, {self.email})'
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), nullable=False)
+    date_posted = db.Column(db.String(10))
+    time_posted = db.Column(db.DateTime, default=datetime.utcnow())
+    user_id = db.Column(db.String(100), db.ForeignKey(
+        'user.id'), nullable=False)
+
+    def __repr__(self) -> str:
+        return f'Todo ({self.title}, {self.date_posted}, {self.time_posted})'
